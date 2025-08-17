2a18ae219160de58b610e96693842f1ccfec43a5
Ali Abbas Chadhar <work.aliabbaschadhar@gmail.com>
2025-08-17 11:31:15 +0500

    starting deploy service logic

---
+++ Vercel-Deploy-Service/src/aws.ts
@@ -0,0 +1,3 @@
+export async function downloadS3Folder(prefix: string) {
+  console.log(prefix)
+}
--- Vercel-Deploy-Service/src/index.ts
+++ Vercel-Deploy-Service/src/index.ts
@@ -1,4 +1,5 @@
 import { createClient } from "redis"
+import { downloadS3Folder } from "./aws"
 
 const subscriber = createClient()
 subscriber.connect()
@@ -7,6 +8,10 @@ async function main() {
   while (true) {
     const response = await subscriber.brPop("build-queue", 0)
     console.log(response)
+
+    // Download the react project from S3 to build the project
+    const id = response?.element
+    await downloadS3Folder(`/output/${id}`)
   }
 }
 
